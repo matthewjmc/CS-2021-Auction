12682b27a79df32b601cc931de0ec3dff762794a
mattheweis <61011279@kmitl.ac.th>
2021-02-23 00:38:39 +0700

    Initial Commit

---
+++ tcpsock.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	stream, err := net.Listen("tcp", ":19530") //Listen at port 19530
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer stream.Close()
+
+	for {
+		con, err := stream.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go requestHandle(con)
+	}
+
+}
+
+func requestHandle(con net.Conn) {
+	for {
+		data, err := bufio.NewReader(con).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(data)
+	}
+	con.Close()
+}
