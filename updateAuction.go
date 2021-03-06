ca97f85444c94b89acf1597e95e9dd48d51c4d14
tagun9921 <tagun9921@hotmail.com>
2021-03-06 18:42:24 +0700

    Update updateAuction.go
    Renaming Variables due to too many underscores.

--- updateAuction.go
+++ updateAuction.go
@@ -15,10 +15,10 @@ import (
 
 ///// Struct Declaration
 //	test := User{
-//		first_name: "Tagun",
-//		last_name:  "Jivasitthikul",
-//		account_id: 15235,
-//		bid_time:   func() string { return time.Now().Format("2006-01-02 15:04:05.00000000000") }
+//		firstName: "Tagun",
+//		lastName:  "Jivasitthikul",
+//		accountID: 15235,
+//		bidTime:   func() string { return time.Now().Format("2006-01-02 15:04:05.00000000000") }
 //	}
 
 ///// Struct is a creation of User Input Data Type.
@@ -37,52 +37,51 @@ func main() {
 	bid1 = createBid(bid1, tagun9921, 500, 11)
 	fmt.Println("wait2")
 	auction = updateAuction(bid1, auction)
-	fmt.Println(auction.curr_max_bid, auction.curr_winner_id)
+	fmt.Println(auction.currMaxBid, auction.currWinnerID)
 
 	bid2 = createBid(bid2, matty, 1000, 22)
 	fmt.Println("wait3")
 	auction = updateAuction(bid2, auction)
-	fmt.Println(auction.curr_max_bid, auction.curr_winner_id)
+	fmt.Println(auction.currMaxBid, auction.currWinnerID)
 
 	bid3 = createBid(bid3, tagun9921, 2000, 33)
 	fmt.Println("wait4")
 	auction = updateAuction(bid3, auction)
-	fmt.Println(auction.curr_max_bid, auction.curr_winner_id)
+	fmt.Println(auction.currMaxBid, auction.currWinnerID)
 
 }
 
 type User struct {
-	first_name string
-	last_name  string
-	account_id uint32
-
-	/*test := User{
-		first_name: "Tagun",
-		last_name:  "Jivasitthikul",
-		account_id: 15235,
-	}
-	createAuction(test)*/
-
+	firstName string
+	lastName  string
+	accountID uint32
 }
+
+/*test := User{
+	firstName: "Tagun",
+	lastName:  "Jivasitthikul",
+	accountID: 15235,
+}
+createAuction(test)*/
 
 func createUser(username User, first string, last string, id uint32) User {
 
 	/*username = User{
-		first_name: first,
-		last_name:  last,
-		account_id: 10,
+		firstName: first,
+		lastName:  last,
+		accountID: 10,
 	}*/
-	/*fmt.Println("This is the account id", username.account_id)
-	fmt.Println("This is the first name", username.first_name)
-	fmt.Println("this is the last name : ", username.last_name)*/
+	/*fmt.Println("This is the account id", username.accountID)
+	fmt.Println("This is the first name", username.firstName)
+	fmt.Println("this is the last name : ", username.lastName)*/
 
-	username.first_name = first
-	username.last_name = last
-	username.account_id = id // need some algorithm to uniquely randomize username id.
+	username.firstName = first
+	username.lastName = last
+	username.accountID = id // need some algorithm to uniquely randomize username id.
 
-	/*fmt.Println("This is the account id", username.account_id)
-	fmt.Println("This is the first name", username.first_name)
-	fmt.Println("this is the last name", username.last_name)*/
+	/*fmt.Println("This is the account id", username.accountID)
+	fmt.Println("This is the first name", username.firstName)
+	fmt.Println("this is the last name", username.lastName)*/
 
 	return username
 }
@@ -90,93 +89,93 @@ func createUser(username User, first string, last string, id uint32) User {
 // func createUser( username string ,
 
 type Auction struct {
-	auction_id      uint32
-	auctioneer_id   uint32
-	item_name       string
-	curr_winner_id  uint32
-	curr_max_bid    uint32
-	bid_step        uint32
-	latest_bid_time time.Time
-	start_time      time.Time
-	end_time        time.Time
-	action_count    uint32
+	auctionID     uint32
+	auctioneerID  uint32
+	itemName      string
+	currWinnerID  uint32
+	currMaxBid    uint32
+	bidStep       uint32
+	latestBidTime time.Time
+	startTime     time.Time
+	endTime       time.Time
+	actionCount   uint32
 }
 
 func createAuction(auction Auction, auctioneer User, id uint32) Auction {
 
-	var item_name string
+	var itemName string
 	fmt.Println("Enter your item name for the auction :")
-	fmt.Scanln(&item_name)
+	fmt.Scanln(&itemName)
 
-	// Consideration of item_name defection
-	//if reflect.TypeOf(item_name) != reflect.TypeOf("") {
+	// Consideration of itemName defection
+	//if reflect.TypeOf(itemName) != reflect.TypeOf("") {
 	//	fmt.Println("The item name must be a string")
-	//	fmt.Scanf("Please re-enter your bidding item : %s", item_name)
+	//	fmt.Scanf("Please re-enter your bidding item : %s", itemNam)
 	//}
 
-	var bid_step uint32
+	var bidStep uint32
 	fmt.Println("Enter your bidding step :")
-	fmt.Scanln(&bid_step)
+	fmt.Scanln(&bidStep)
 
 	// Consideration of bidding step defection
 	//var integer32 uint32 = 4
-	//if reflect.TypeOf(item_name) != reflect.TypeOf(integer32) {
+	//if reflect.TypeOf(itemName) != reflect.TypeOf(integer32) {
 	//	fmt.Println("The bidding step must be a positive integer")
-	//	fmt.Scanf("Please re-enter your bidding step : %s", bid_step)
+	//	fmt.Scanf("Please re-enter your bidding step : %s", bidStep)
 	//}
 
-	var init_bid uint32
+	var initBid uint32
 	fmt.Println("Enter your initial starting price :")
-	fmt.Scanln(&init_bid)
+	fmt.Scanln(&initBid)
 
 	var duration time.Duration
 	fmt.Println("Enter your auction duration in hours :")
 	fmt.Scanln(&duration)
 
 	auction = Auction{
-		auction_id:      id,
-		auctioneer_id:   auctioneer.account_id,
-		item_name:       item_name,
-		curr_winner_id:  auctioneer.account_id, //
-		curr_max_bid:    init_bid,              //
-		bid_step:        bid_step,
-		latest_bid_time: time.Now(), //
-		start_time:      time.Now(),
-		end_time:        time.Now().Add(duration * time.Hour),
-		action_count:    0,
+		auctionID:     id,
+		auctioneerID:  auctioneer.accountID,
+		itemName:      itemName,
+		currWinnerID:  auctioneer.accountID,
+		currMaxBid:    initBid,
+		bidStep:       bidStep,
+		latestBidTime: time.Now(),
+		startTime:     time.Now(),
+		endTime:       time.Now().Add(duration * time.Hour),
+		actionCount:   0,
 	}
 	return auction
 }
 
 type Bid struct {
-	bidding_id uint32
-	bidder_id  uint32
-	bid_price  uint32
-	bid_time   time.Time
+	biddingID uint32
+	bidderID  uint32
+	bidPrice  uint32
+	bidTime   time.Time
 }
 
-func createBid(bid_action Bid, user User, price uint32, id uint32) Bid {
-	bid_action = Bid{
-		bidding_id: id,
-		bidder_id:  user.account_id,
-		bid_price:  price,
-		bid_time:   time.Now(),
+func createBid(bidAction Bid, user User, price uint32, id uint32) Bid {
+	bidAction = Bid{
+		biddingID: id,
+		bidderID:  user.accountID,
+		bidPrice:  price,
+		bidTime:   time.Now(),
 	}
-	return bid_action
+	return bidAction
 }
 
 func updateAuction(b Bid, a Auction) Auction {
 
-	if b.bid_price-a.curr_max_bid >= a.bid_step {
+	if b.bidPrice-a.currMaxBid >= a.bidStep {
 
-		if b.bid_price > a.curr_max_bid {
+		if b.bidPrice > a.currMaxBid {
 
-			if b.bid_time.After(a.latest_bid_time) {
+			if b.bidTime.After(a.latestBidTime) {
 
-				a.curr_max_bid = b.bid_price
-				a.curr_winner_id = b.bidder_id
-				a.latest_bid_time = b.bid_time
-				a.action_count += 1
+				a.currMaxBid = b.bidPrice
+				a.currWinnerID = b.bidderID
+				a.latestBidTime = b.bidTime
+				a.actionCount++
 
 			}
 
