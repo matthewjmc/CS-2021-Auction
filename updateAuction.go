7a282bc7404a19e2d0a33ca33bca5e4749b734cb
tagun9921 <tagun9921@hotmail.com>
2021-03-07 03:18:06 +0700

    Simple mock testing
    - Structs creating functions reinterpret.
    - New updateAuction function.
    - Synchronous user mockup test.

--- updateAuction.go
+++ updateAuction.go
@@ -14,18 +14,11 @@ import (
 //	currentTime := time.Now()
 //	fmt.Printf("The datetime data type is %T\n", currentTime.Format("2006-01-02 15:04:05.000000000"))
 
-///// Struct Declaration
-//	test := User{
-//		firstName: "Tagun",
-//		lastName:  "Jivasitthikul",
-//		accountID: 15235,
-//		bidTime:   func() string { return time.Now().Format("2006-01-02 15:04:05.00000000000") }
-//	}
-
 ///// Struct is a creation of User Input Data Type.
 
-func main() {
-	Tagun := User{
+/*
+
+Tagun := User{
 		firstName: "Tagun",
 		lastName:  "Jivasitthikul",
 		accountID: 1,
@@ -36,6 +29,11 @@ func main() {
 	go fmt.Println(updateDB(bid1))
 
 	fmt.Scanln()
+
+*/
+
+func main() {
+	simpleMockTest()
 }
 
 type User struct {
@@ -51,7 +49,7 @@ type User struct {
 }
 createAuction(test)*/
 
-func createUser(username User, first string, last string, id uint32) User {
+func createUser(first string, last string, id uint32) User {
 
 	/*username = User{
 		firstName: first,
@@ -62,18 +60,16 @@ func createUser(username User, first string, last string, id uint32) User {
 	fmt.Println("This is the first name", username.firstName)
 	fmt.Println("this is the last name : ", username.lastName)*/
 
-	username.firstName = first
-	username.lastName = last
-	username.accountID = id // need some algorithm to uniquely randomize username id.
+	account := User{firstName: first}
+	account.lastName = last
+	account.accountID = id // need some algorithm to uniquely randomize username id.
 
 	/*fmt.Println("This is the account id", username.accountID)
 	fmt.Println("This is the first name", username.firstName)
 	fmt.Println("this is the last name", username.lastName)*/
 
-	return username
+	return account
 }
-
-// func createUser( username string ,
 
 type Auction struct {
 	auctionID     uint32
@@ -88,39 +84,33 @@ type Auction struct {
 	actionCount   uint32
 }
 
-func createAuction(auction Auction, auctioneer User, id uint32) Auction {
+func createAuction(auctioneer User) Auction {
 
-	var itemName string
-	fmt.Println("Enter your item name for the auction :")
-	fmt.Scanln(&itemName)
+	var id uint32 = 1111111111
+	var itemName string = "test"
+	var initBid uint32 = 200
+	var bidStep uint32 = 50
+	var duration time.Duration = 1
+	/*
+		var itemName string
+		fmt.Println("Enter your item name for the auction :")
+		fmt.Scanln(&itemName)
 
-	// Consideration of itemName defection
-	//if reflect.TypeOf(itemName) != reflect.TypeOf("") {
-	//	fmt.Println("The item name must be a string")
-	//	fmt.Scanf("Please re-enter your bidding item : %s", itemNam)
-	//}
+		var bidStep uint32
+		fmt.Println("Enter your bidding step :")
+		fmt.Scanln(&bidStep)
 
-	var bidStep uint32
-	fmt.Println("Enter your bidding step :")
-	fmt.Scanln(&bidStep)
+		var initBid uint32
+		fmt.Println("Enter your initial starting price :")
+		fmt.Scanln(&initBid)
 
-	// Consideration of bidding step defection
-	//var integer32 uint32 = 4
-	//if reflect.TypeOf(itemName) != reflect.TypeOf(integer32) {
-	//	fmt.Println("The bidding step must be a positive integer")
-	//	fmt.Scanf("Please re-enter your bidding step : %s", bidStep)
-	//}
+		var duration time.Duration
+		fmt.Println("Enter your auction duration in hours :")
+		fmt.Scanln(&duration)
+	*/
 
-	var initBid uint32
-	fmt.Println("Enter your initial starting price :")
-	fmt.Scanln(&initBid)
-
-	var duration time.Duration
-	fmt.Println("Enter your auction duration in hours :")
-	fmt.Scanln(&duration)
-
+	auction := Auction{auctionID: id}
 	auction = Auction{
-		auctionID:     id,
 		auctioneerID:  auctioneer.accountID,
 		itemName:      itemName,
 		currWinnerID:  auctioneer.accountID,
@@ -141,25 +131,42 @@ type Bid struct {
 	bidTime   time.Time
 }
 
-func createBid(bidAction Bid, user User, price uint32, id uint32) Bid {
-	bidAction = Bid{
+func createBid(user User, price uint32) Bid {
+	var id uint32 = 2344
+	bid := Bid{}
+	bid = Bid{
 		biddingID: id,
 		bidderID:  user.accountID,
 		bidPrice:  price,
 		bidTime:   time.Now(),
 	}
-	return bidAction
+	return bid
 }
 
-/*func updateAuction(b Bid, a Auction) Auction {
-	var difference = b.bidPrice - a.currMaxBid
-	if b.bidPrice > a.currMaxBid && difference >= a.bidStep && b.bidTime.After(a.latestBidTime) {
+func updateAuction(b Bid, a Auction) Auction {
 
+	var start time.Time = time.Now()
+	//fmt.Println("The initial time that the auction update process caused by", b.biddingID, "is being called is", time.Now())
+
+	if b.bidTime.After(a.endTime) {
+		fmt.Printf("Auction %d has already end. Bid placement is canceled\n", a.auctionID)
+		return a
 	}
 
-}*/
+	var difference = b.bidPrice - a.currMaxBid
+	if b.bidPrice > a.currMaxBid && difference >= a.bidStep && b.bidTime.After(a.latestBidTime) {
+		a.currMaxBid = b.bidPrice
+		a.currWinnerID = b.bidderID
+		a.latestBidTime = b.bidTime
+	}
 
-// hihihihi
+	var end time.Time = time.Now()
+	//fmt.Println("The final time that the auction update process caused by", b.biddingID, "is ended is", time.Now())
+
+	fmt.Println(end.Sub(start))
+
+	return a
+}
 
 func updateDB(x interface{}) string {
 	// get the input items to be transferred to the database
@@ -167,6 +174,38 @@ func updateDB(x interface{}) string {
 }
 
 func displayAction(x interface{}) string {
-	// get the input items to be transferred to the database
+	// get the input items to be transferred through TCP sockets
 	return reflect.TypeOf(x).String()
+}
+
+func mockUserCreate() []User {
+
+	var tagun9921 = createUser("Tagun", "Jivasitthikul", 9921)
+	var lengeiei = createUser("Katisak", "Jiangjaturapat", 1547)
+	var mattfatt = createUser("Matthew", "McMullin", 7812)
+	var luckyS = createUser("Vorachat", "Somsuay", 3443)
+
+	fmt.Println(tagun9921)
+	fmt.Println(lengeiei)
+	fmt.Println(mattfatt)
+	fmt.Println(luckyS)
+
+	mockUserArray := []User{tagun9921, lengeiei, mattfatt, luckyS}
+	return mockUserArray
+}
+
+func simpleMockTest() {
+
+	var userArray = mockUserCreate()
+	//fmt.Println("\n", userArray)
+	//fmt.Println(userArray[0])
+
+	var testAuction = createAuction(userArray[0]) // tagun9921 creates an auction.
+	// The testing auction has the initial bid of 200, bid steps of 50 and duration of 1 hour.
+	// The possible first bid suppose to have at least 260
+
+	var bid1 = createBid(userArray[1], 300)
+	//fmt.Println(bid1.biddingID, bid1.bidPrice, bid1.bidTime, bid1.bidderID)
+
+	updateAuction(bid1, testAuction)
 }
