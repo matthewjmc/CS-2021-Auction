82c22a4b74b0a00e150655efc58a321e69ac2045
Matthew <matthew@mcmullin.one>
2021-03-07 07:58:22 +0000

    update Client Driver

--- ClientDriver/clientSpawn.go
+++ ClientDriver/clientSpawn.go
@@ -4,26 +4,32 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"bufio"
 )
 
 func main() {
-	n := 1
-	for n < 1000 {
-		c, err := net.Dial("tcp", "10.0.59.136:19530")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		go handleCon(c)
-		fmt.Println(n)
-		n++
-	}
-	time.Sleep(100000000 * time.Second)
+	handleCon()
+
 }
 
-func handleCon(c net.Conn) {
+func handleCon() {
+	c, _ := net.Dial("tcp", "10.0.59.140:7777")
+	err := c.(*net.TCPConn).SetKeepAlive(true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = c.(*net.TCPConn).SetKeepAlivePeriod(30 * time.Second)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	for {
-		fmt.Fprintf(c, time.Nanosecond.String()+"\n") //Print to server
-		time.Sleep(1 * time.Second)
+		fmt.Fprintf(c,"TestTestTest\n")
+		data, _ := bufio.NewReader(c).ReadString('\n')
+		fmt.Println("From -->", data)
+		time.Sleep(1*time.Second)
+		
+		
 	}
 }
