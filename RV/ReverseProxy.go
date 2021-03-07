3f2f5144d6829a927734ab2dac7d7f5131f4958b
viewnts <viewntp@pop-os.localdomain>
2021-03-08 01:32:27 +0700

    Can recieve many from Client

--- RV/ReverseProxy.go
+++ RV/ReverseProxy.go
@@ -1,74 +1,42 @@
 package main
 
 import (
-	"bytes"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
-	"time"
 )
 
 func main() {
+	front := "localhost:7000"
+	back := "localhost:9000"
+
+	In, err := net.Listen("tcp", front)
+	if err != nil {
+		log.Fatalf("failed to setup listener: %v", err)
+	}
+
+	log.Printf("listening on %s, balancing %s", front, back)
+	log.Println("//////////////////LISTENING//////////////////")
+
 	for {
-		In, err := net.Listen("tcp", "localhost:7777") //prox
-
+		Inconn, err := In.Accept()
 		if err != nil {
-			log.Fatalf("failed to setup listener %v", err)
+			log.Printf("failed to accept: %s", err)
+			continue
 		}
-
-		log.Println("Accepted from," + "localhost:7777")
-		log.Println("//////////////WATING FOR CONNECTION//////////////")
-
-		for {
-
-			Inconn, err := In.Accept()
-			if err != nil {
-				log.Fatalf("failed to accept listener %v", err)
-			}
-			log.Print("Connection Accepted")
-
-			go reverse_proxy(Inconn)
-		}
+		go ReProx(Inconn, back)
 	}
 }
 
-func reverse_proxy(src net.Conn) {
-	//setup reader
-	defer src.Close()
-	err := src.SetDeadline(time.Now().Add(5 * time.Second))
-
+func ReProx(src net.Conn, server string) {
+	dst, err := net.Dial("tcp", server)
 	if err != nil {
-		log.Fatalf("No Reverse Proxy deadline %v", err)
+		src.Close()
+		log.Printf("failed to dial %s: %s", server, err)
+		return
 	}
 
-	//Buffer
-	requestBuf := make([]byte, 512)
-	reqLen, _ := src.Read(requestBuf)
+	io.Copy(dst, src)
+	io.Copy(src, dst)
 
-	_ = reqLen
-
-	log.Println("We Sent Request:", string(requestBuf))
-
-	//read the request
-
-	dst, _ := net.Dial("tcp", "localhost:9000")
-
-	defer dst.Close()
-	err = dst.SetDeadline(time.Now().Add(5 * time.Second))
-
-	if err != nil {
-		log.Fatalf("No Reverse Proxy deadline %v", err)
-	}
-
-	log.Print("ReverseProxy Connected")
-
-	var backendBuf bytes.Buffer
-	ndst := io.TeeReader(dst, &backendBuf)
-	io.Copy(dst, bytes.NewReader(requestBuf)) //server, prox
-	io.Copy(src, ndst)
-
-	//read response
-	backendBytes, _ := ioutil.ReadAll(&backendBuf)
-	log.Println("We got response:", string(backendBytes))
 }
