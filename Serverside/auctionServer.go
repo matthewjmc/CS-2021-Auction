efc4525d7c5a83757649bba5af7990a486eaa188
Matthew <matthew@mcmullin.one>
2021-03-07 07:53:50 +0000

    update auction server

---
+++ Serverside/auctionServer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	stream, err := net.Listen("tcp", ":19530") //Listen at port 19530
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer stream.Close()
+	var count int64 = 1
+	for {
+		con, err := stream.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Conn Number:", count,"-->",con.RemoteAddr().String())
+		go requestHandle(con, count)
+		count++
+		fmt.Println(count)
+	}
+
+}
+
+func requestHandle(con net.Conn, cnt int64) {
+	for {
+		data, err := bufio.NewReader(con).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Println("From", con.RemoteAddr().String(), "-->", data)
+		go returnData(con)
+	}
+	con.Close()
+}
+
+func returnData(con net.Conn){
+	fmt.Fprintf(con, con.RemoteAddr().String()+"\n")
+}
