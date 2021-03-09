f3cb69adfcf22d41579190f19cd859c35e8a6478
Matthew <matthew@mcmullin.one>
2021-03-09 15:30:47 +0000

    hello

--- go_get_sleep.sh
+++ go_get_sleep.sh
@@ -3,8 +3,6 @@
 echo "Go Get Sleep Pushing into Github Now"
 
 read -p "Commit message:" uservar
-echo "<--------------Sleeping Now Commit and Pushing to Github-------------->"
-
 git commit -a -m $uservar
 git push origin Matthew
 
