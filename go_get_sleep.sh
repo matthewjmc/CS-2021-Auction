b5413c4ad5d3c0d2cfb4d361010d42432b4299a2
Matthew <matthew@mcmullin.one>
2021-03-09 06:41:22 +0000

    <--------------Sleeping Now Commit and Pushing to Github-------------->

---
+++ go_get_sleep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Go Get Sleep Pushing into Github Now"
+
+git commit -a -m "<--------------Sleeping Now Commit and Pushing to Github-------------->"
+git push origin Matthew
+
+echo "Choo Choo"
+sl
+
+
+
+
