1890aa80acac97074ac470b847f17db30e3c0967
maeluenie <maeluenie@gmail.com>
2021-03-09 21:44:48 +0700

    Planning out loadBalance.go

---
+++ loadBalance.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+// TODO - Data structure storing backend info
+
+// TODO - Manager (all frontend responsibility) TCP Server listening on a port
+
+// TODO - Least Connection routes request to backend that have least number of connection
+
+// TODO - RoundRobin request to backend with round robin fashion
