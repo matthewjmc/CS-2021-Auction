a24377bf08afd4bb45d7b99966c60d9a3abba19d
Matthew <matthew@mcmullin.one>
2021-03-09 17:48:46 +0000

    update Client

--- Serverside/auctionServer.go
+++ Serverside/auctionServer.go
@@ -4,7 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"encoding/json"
 )
+type Package struct{  // Data Sent and Received From user
+	AuctionID int
+	UserID int
+	Command  string
+	Data struct {
+		Item string
+		Value int
+	}
+}
+
+type Auction struct{ //Auctions Running at one time
+	AuctionID int64
+	ConnectedClients []net.Conn
+}
+
+var aucSessions = []Auction{} //All Connected Auctions
+var ptSession = &aucSessions
+
 
 func main() {
 	stream, err := net.Listen("tcp", ":19530") //Listen at port 19530
@@ -13,23 +32,26 @@ func main() {
 		return
 	}
 	defer stream.Close()
-	var count int64 = 1
 	for {
 		con, err := stream.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		//fmt.Println("Conn Number:", count,"-->",con.RemoteAddr().String())
-		go requestHandle(con, count)
-		count++
-		fmt.Println(count)
+		go requestHandle(con)
 	}
 }
 
-func requestHandle(con net.Conn, cnt int64) {
+func requestHandle(con net.Conn) {
+	var state bool = false
+	var received Package //Data Received From User to be decoded to Struct
+
 	for {
-		_, err := bufio.NewReader(con).ReadString('\n')
+		rawdata,err := bufio.NewReader(con).ReadString('\n')
+		if !state{
+			json.Unmarshal([]byte(rawdata), &received)
+			addUsr(con,received.AuctionID)
+		}
 		if err != nil {
 			fmt.Println(err)
 			break
@@ -41,4 +63,8 @@ func requestHandle(con net.Conn, cnt int64) {
 
 func returnData(con net.Conn){
 	fmt.Fprintf(con, con.RemoteAddr().String()+"\n")
+}
+
+func addUsr(con net.Conn, AuctionID int){
+	if 
 }
