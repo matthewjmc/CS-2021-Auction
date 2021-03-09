b295c87bd409d32340c75b1f44f1a34857797733
Matthew <matthew@mcmullin.one>
2021-03-09 06:28:28 +0000

    'Pushing Everything into Git Time to Sleep'

--- get_sleep.go
+++ get_sleep.go
@@ -7,20 +7,9 @@ import (
 
 func main() {
 	fmt.Println("<---------- Pushing Code to Github Now ---------->")
-	cmd := exec.Command("git commit -m", "'Finished for the Day pushing to Github now'")
-	if err := cmd.Run(); err != nil {
+	data,err := exec.Command("git","commit","-a","-m","'Pushing Everything into Git Time to Sleep'").Output()
+	if err != nil{
 		fmt.Println(err)
-
 	}
-	cmd = exec.Command("git push origin Matthew")
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-
-	}
-	fmt.Println("<---------- Bye Bye ---------->")
-	cmd = exec.Command("sl")
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-
-	}
+	fmt.Println(string(data))
 }
