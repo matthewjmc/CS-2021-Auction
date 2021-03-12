520b9dda71d5157521d9185773794fdf52da28db
Matthew <matthew@mcmullin.one>
2021-03-12 20:03:12 +0000

    client Docker

--- ClientDriver/clientSpawn.go
+++ ClientDriver/clientSpawn.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net"
 	"time"
-	"bufio"
+	//"bufio"
 	"runtime"
 	"os"
 	"strconv"
@@ -25,10 +25,10 @@ func main() {
 	arguments := os.Args
 	args,_ := strconv.Atoi(arguments[1])
 	runtime.GOMAXPROCS(4) //Use 4 Cores
-	n:=1
+	n:=0
 	for n<args{
 		user := Package{}
-		user.AuctionID = rand.Intn(20 - 1) + 1
+		user.AuctionID = rand.Intn(10 - 1) + 1
 		user.UserID = rand.Intn(2000 - 1) + 1
 		user.Data.Item = "Price"
 		user.Data.Value = rand.Intn(100000 - 10) + 10
@@ -55,14 +55,15 @@ func handleCon(data Package) {
 	//Convert Struct to JSON Document
 	var jsonData []byte
 	jsonData, err = json.Marshal(data)
+	fmt.Println("AuctionID:",data.AuctionID)
 	if err != nil {
     	fmt.Println(err)
 	}
 	//Transmit and Receive
 	for {
 		fmt.Fprintf(connection,string(jsonData)+"\n")
-		data, _ := bufio.NewReader(connection).ReadString('\n')
-		fmt.Println("From -->", data)
+		//data, _ := bufio.NewReader(connection).ReadString('\n')
+		//fmt.Println("From -->", data)
 		time.Sleep(1*time.Second)
 	}
 	connection.Close()
