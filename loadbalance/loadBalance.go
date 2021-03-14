b8322f34e26338ded2061863c88e1ddbcdbd3e9e
maeluenie <maeluenie@gmail.com>
2021-03-15 00:01:14 +0700

    create route

--- loadbalance/loadBalance.go
+++ loadbalance/loadBalance.go
@@ -1,18 +1,10 @@
 package main
 
-func main() {
-
-}
-
-// TODO - Data structure storing backend info
-type Server struct {
-	Route string
-	Alive bool
-}
-
+// Data structure storing backend info
 type ServerList struct {
-	Servers []Server
+	Servers []string
 	Latest  int
+	Alive   bool
 }
 
 // TODO - Manager (all frontend responsibility) TCP Server listening on a port
@@ -20,3 +12,12 @@ type ServerList struct {
 // TODO - Least Connection routes request to backend that have least number of connection
 
 // TODO - RoundRobin request to backend with round robin fashion
+func (server *ServerList) route() string {
+	i := server.Latest % len(server.Servers)
+	server.Latest++
+	return server.Servers[i]
+}
+
+func main() {
+
+}
