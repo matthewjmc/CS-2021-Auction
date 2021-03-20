143274dbada352aa28a3615c2eb125f766695bf1
root <matthew@mcmullin.one>
2021-03-20 18:16:43 +0000

    Merger Matthew Tagun Katisak

--- ClientDriver/clientSpawn.go
+++ ClientDriver/clientSpawn.go
@@ -4,34 +4,37 @@ import (
 	"fmt"
 	"net"
 	"time"
+
 	//"bufio"
-	"runtime"
-	"os"
-	"strconv"
 	"encoding/json"
 	"math/rand"
+	"os"
+	"runtime"
+	"strconv"
 )
-type Package struct{
+
+type Package struct {
 	AuctionID int
-	UserID int
-	Command  string
-	Data struct {
-		Item string
+	UserID    int
+	Command   string
+	Data      struct {
+		Item  string
 		Value int
 	}
 }
 
 func main() {
 	arguments := os.Args
-	args,_ := strconv.Atoi(arguments[1])
+	args, _ := strconv.Atoi(arguments[1])
 	runtime.GOMAXPROCS(4) //Use 4 Cores
-	n:=0
-	for n<args{
+	n := 0
+	for n < args {
 		user := Package{}
-		user.AuctionID = rand.Intn(10 - 1) + 1
-		user.UserID = rand.Intn(2000 - 1) + 1
+		user.AuctionID = rand.Intn(10-1) + 1
+		user.UserID = rand.Intn(2000-1) + 1
 		user.Data.Item = "Price"
-		user.Data.Value = rand.Intn(100000 - 10) + 10
+		user.Data.Value = rand.Intn(100000-10) + 10
+
 		go handleCon(user)
 		n++
 	}
@@ -42,6 +45,7 @@ func main() {
 
 func handleCon(data Package) {
 	connection, _ := net.Dial("tcp", "10.0.59.139:19530")
+	defer connection.Close()
 	err := connection.(*net.TCPConn).SetKeepAlive(true)
 	if err != nil {
 		fmt.Println(err)
@@ -55,16 +59,16 @@ func handleCon(data Package) {
 	//Convert Struct to JSON Document
 	var jsonData []byte
 	jsonData, err = json.Marshal(data)
-	fmt.Println("AuctionID:",data.AuctionID)
+	fmt.Println("AuctionID:", data.AuctionID)
 	if err != nil {
-    	fmt.Println(err)
+		fmt.Println(err)
 	}
 	//Transmit and Receive
 	for {
-		fmt.Fprintf(connection,string(jsonData)+"\n")
+		fmt.Fprintf(connection, string(jsonData)+"\n")
 		//data, _ := bufio.NewReader(connection).ReadString('\n')
 		//fmt.Println("From -->", data)
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 	}
-	connection.Close()
+
 }
