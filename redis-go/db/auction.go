32f87f7d21889f20e077c2ae9677b0d2194136c2
maeluenie <maeluenie@gmail.com>
2021-03-20 19:32:40 +0700

    add redis module

--- redis-go/db/auction.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type Auction struct {
-	Command     string `json:"command" binding:"required"`
-	AuctionID   int    `json:"auctionid" binding:"required"`
-	ServerIP    string `json:"ipaddr"`
-	Description string `json:"desc"`
-	ConnUsers   int    `json:"connected users"`
-}
