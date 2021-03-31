847a7e70c64326a6e8f51ba6be963c0acc2a2e7d
maeluenie <maeluenie@gmail.com>
2021-03-31 19:24:19 +0700

    track and store only cpu usage values

--- Getstat/Listen/server.go
+++ Getstat/Listen/server.go
@@ -5,39 +5,72 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"regexp"
 	"strings"
 	"time"
+	//"sync"
+	//"reflect"
+	//rv "load_balance/reverseproxy"
 )
+
+var S1_Usage, S2_Usage string
 
 // this will locate on the loadbalance server to listen for cpu usage from S1/S2
 // every period of time set
 func main() {
+	//var wg sync.WaitGroup
+	conn, err := net.Listen("tcp4", ":19530")
+	if err != nil {
+		fmt.Println(err)
+	}
 	for {
-		conn, err := net.Listen("tcp4", ":19530")
-		if err != nil {
-			fmt.Println(err)
-		}
 		c, err := conn.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		for {
-			netData, err := bufio.NewReader(c).ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			if strings.TrimSpace(string(netData)) == "STOP" {
-				fmt.Println("Exiting TCP server!")
-				return
-			}
-			fmt.Print(string(netData))
-			t := time.Now()
-			myTime := t.Format(time.RFC3339) + "\n"
-			c.Write([]byte(myTime))
+		//wg.Add(1)
+		go getStat(c)
+		//wg.Wait()
+	}
+
+}
+
+func getStat(conn net.Conn) {
+	defer conn.Close()
+	for {
+		netData, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
 		}
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+		}
+
+		usage := string(netData)
+		fmt.Println(usage)
+		if usage[12] == 116 {
+			re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+			submatchall := re.FindAllString(usage, -1)
+			for _, element := range submatchall {
+				S2_Usage = element
+				fmt.Println(S2_Usage)
+			}
+		}
+		if usage[12] == 111 {
+			re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+			submatchall := re.FindAllString(usage, -1)
+			for _, element := range submatchall {
+				S1_Usage = element
+				fmt.Println(S1_Usage)
+			}
+		}
+
+		t := time.Now()
+		myTime := t.Format(time.RFC3339) + "\n"
+		conn.Write([]byte(myTime))
+		continue
 	}
 }
 
