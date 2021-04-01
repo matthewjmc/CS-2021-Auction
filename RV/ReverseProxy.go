2c68bc8fe2b57bc6d80abfe52e1b866c0658fab4
viewnts <44019863+viewnts@users.noreply.github.com>
2021-04-01 20:14:48 +0700

    Update ReverseProxy.go
    debug the problems that the auction server couldn't connect to any client.

--- RV/ReverseProxy.go
+++ RV/ReverseProxy.go
@@ -1,57 +1,63 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
+        "io"
+        "log"
+        "net"
+        "fmt"
 )
 
 func main() {
-	front := "19530"
-	back := "com2.mcmullin.org:19530"
-	In, err := net.Listen("tcp", front)
-	if err != nil {
-		log.Fatalf("failed to setup listener: %v", err)
-	}
+        front := ":19530"
+        back := "com1.mcmullin.org:19530"
+        In, err := net.Listen("tcp4", front)
+        if err != nil {
+                log.Fatalf("failed to setup listener: %v", err)
+        }
 
-	log.Printf("listening on %s, balancing %s", front, back)
-	log.Println("//////////////////LISTENING//////////////////")
+        log.Printf("listening on %s, balancing %s", front, back)
+        log.Println("//////////////////LISTENING//////////////////")
+        i := 1
+        for {
+                Inconn, err := In.Accept()
+                fmt.Println(i)
+                go ReProx(Inconn, back)
+                if err != nil {
+                        log.Printf("failed to accept: %s", err)
+                        continue
+                }
+                i++
 
-	for {
-		Inconn, err := In.Accept()
-		go ReProx(Inconn, back)
-		if err != nil {
-			log.Printf("failed to accept: %s", err)
-			continue
-		}
-
-	}
+        }
 }
 func copy(src net.Conn, dst net.Conn, stop chan bool) {
-	io.Copy(dst, src)
-	dst.Close()
-	src.Close()
-	stop <- true
-	return
+        io.Copy(dst, src)
+        dst.Close()
+        src.Close()
+        stop <- true
+        return
 }
+
 
 func ReProx(src net.Conn, server string) {
 
-	dst, err := net.Dial("tcp", server)
-	if err != nil {
-		src.Close()
-		log.Printf("failed to dial %s: %s", server, err)
-		return
-	}
+        dst, err := net.Dial("tcp4", server)
+        if err != nil {
+                src.Close()
+                log.Printf("failed to dial %s: %s", server, err)
+                return
+        }
 
-	stop := make(chan bool)
+        stop := make(chan bool)
 
-	go copy(dst, src, stop)
-	go copy(src, dst, stop)
+        go copy(dst, src, stop)
+        go copy(src, dst, stop)
 
-	select {
-	case <-stop:
-		return
-	}
+        select {
+        case <-stop:
+                return
+        }
 
 }
+
+
